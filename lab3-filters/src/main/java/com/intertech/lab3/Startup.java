f095e1867a60f30aef41cf3edcaba9d60f3a6cdc
Salvatore Luciano <salvatore.luciano@soprasteria.com>
2021-11-18 12:16:11 +0100

    esercizion sui filtri

---
+++ lab3-filters/src/main/java/com/intertech/lab3/Startup.java
@@ -0,0 +1,16 @@
+package com.intertech.lab3;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Startup {
+
+	@SuppressWarnings({ "resource", "unused" })
+	// @SuppressWarnings({ "resource" })
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"/META-INF/spring/si-components.xml");
+		while (true) {
+		}
+	}
+
+}
